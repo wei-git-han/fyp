a6bfa376f22675ea92fe12df3cdfc169fc6f43d4
lixue <lixue>
2020-08-20 13:41:44 +0800

    排行信息---软件数据页面开发及数据模拟

--- src/main/resources/static/app/fyp/rcgzIframe/js/zwqk_iframe.js
+++ src/main/resources/static/app/fyp/rcgzIframe/js/zwqk_iframe.js
@@ -10,29 +10,32 @@ var pageModule = function () {
 					arryHtml+=  ' <div class="tab3_title">'+o.text+'</div>'+
 								'	<div class="tab3_main">';
 								$.each(o.children,function(index,item){
-									console.log(o.children)
 									var statusHtml = "";
 									var statusColor = "";
-									if(item.status == 1){
-										statusHtml ="(在线中)";
-										statusColor = "#21B756";
+									switch (item.status){
+										case "0":
+											statusHtml ="已请假...";
+											statusColor = "#D3D43E";
+											break;
+										case "1":
+											statusHtml ="在线中...";
+											statusColor = "#21B756";
+											break;
 									}
-									if(item.status == 0){
-										statusHtml ="(请假中)";
-										statusColor = "#D3D43E";
-									}
+									
 									arryHtml+=  '<div class="user_card">'+
 												'	<img src="../../common/images/headBlue.png" />'+
 												'	<div>'+
-												'		<span class="userName">'+item.text+'</span><span class="status" style="color:'+statusColor+'">'+statusHtml+'</span><br/>'+
-												'		<a class="more" id="'+item.id+'"><i class="fa fa-chevron-circle-down"></i> 更多操作</a>'+
+												'		<span class="userName">'+item.text+'</span><span>('+item.rose+')</span><br/>'+
+												'		<span class="status" style="color:'+statusColor+'">'+statusHtml+'</span>'+
+												/* '		<a class="more" id="'+item.id+'"><i class="fa fa-chevron-circle-down"></i> 更多操作</a>'+ */
 												'	</div>'+
 												'</div>';
 									})
 									arryHtml+=	'</div>';
 				});
-				var moreHtml = '<button class="moreBtn">查看更多详情&nbsp;></button>';
-				$("#zwqk").html(arryHtml+moreHtml);
+				/* var moreHtml = '<button class="moreBtn">查看更多详情&nbsp;></button>'; */
+				$("#zwqk").html(arryHtml);
 			}
 		})
 	}
