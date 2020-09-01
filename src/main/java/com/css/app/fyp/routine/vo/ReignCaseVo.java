e83acebbc9a5a04c04f1c356bfacc4b82d88bd0c
gongan <15313571632@163.com>
2020-09-01 15:40:58 +0800

    Merge branches '20200813_show_fyp_dev' and 'product' of http://172.16.201.140:3000/ZF_BGPT_APP/zf_new_fyp_app into 20200813_show_fyp_dev
    # Conflicts:
    #	src/main/java/com/css/addbase/constant/AppInterfaceConstant.java
    #	src/main/java/com/css/base/utils/CrossDomainUtil.java

---
+++ src/main/java/com/css/app/fyp/routine/vo/ReignCaseVo.java
@@ -0,0 +1,35 @@
+package com.css.app.fyp.routine.vo;
+
+public class ReignCaseVo {
+
+    //ID
+    private String reignCaseId;
+    //名字
+    private String reignCaseName;
+    //状态
+    private String reignCaseType;
+
+    public String getReignCaseId() {
+        return reignCaseId;
+    }
+
+    public void setReignCaseId(String reignCaseId) {
+        this.reignCaseId = reignCaseId;
+    }
+
+    public String getReignCaseName() {
+        return reignCaseName;
+    }
+
+    public void setReignCaseName(String reignCaseName) {
+        this.reignCaseName = reignCaseName;
+    }
+
+    public String getReignCaseType() {
+        return reignCaseType;
+    }
+
+    public void setReignCaseType(String reignCaseType) {
+        this.reignCaseType = reignCaseType;
+    }
+}
