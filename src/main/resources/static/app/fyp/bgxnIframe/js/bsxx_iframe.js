e8e372d5fbc49acc2ea5c340b2e09502e83f7bd1
lixue <lixue>
2020-09-07 10:01:15 +0800

    排行数据对接前端完善

--- src/main/resources/static/app/fyp/bgxnIframe/js/bsxx_iframe.js
+++ src/main/resources/static/app/fyp/bgxnIframe/js/bsxx_iframe.js
@@ -1,5 +1,5 @@
 var listurl = {"url": "/app/fyp/manageThing/weeklyTable","dataType": "text"};
-var deptTreeUrl = {"url":"/app/base/dept/tree","dataType":"text"}; //单位树--待定
+var deptTreeUrl = {"url":"/app/base/dept/tree","dataType":"text"}; //单位树
 var grid = null;
 
 var pageModule = function() {
---
+++ src/main/resources/static/app/fyp/rcgzIframe/js/gzzb_add.js
@@ -0,0 +1,89 @@
+var deptTreeUrl = {"url":"/app/base/dept/tree","dataType":"text"}; //单位树
+var saveUrl = "";
+var flag=getUrlParam("flag")||"";//上午下午区分
+var pageModule = function(){
+	//单位树
+	var initUnitTree = function(){
+		$("#deptName").createSelecttree({
+			url :deptTreeUrl,
+			width : '100%',
+			success : function(data, treeobj) {},
+			selectnode : function(e, data) {
+				$("#deptName").val(data.node.text);
+				$("#deptId").val(data.node.id);
+			}
+		});
+	}
+	
+	
+	var initother = function(){
+		$(".timepicker-24").timepicker({
+		    language:"zh-CN",
+		    autoclose: true,
+		    minuteStep: 5,
+		    showSeconds: false,
+		    showMeridian: false
+		});
+
+		$(".timepicker").parent(".input-group").on("click", ".input-group-btn", function(e){
+		    e.preventDefault();
+		    $(this).parent(".input-group").find(".timepicker").timepicker("showWidget");
+		});
+		
+		
+		$("#commentForm").validate({
+			ignore:'',
+		    submitHandler: function() {
+			    var elementarry = [""];
+				var paramdata = getformdata(elementarry);
+				$ajax({
+					url:saveUrl,
+					data:paramdata,
+					type:'post',
+					success:function(data){
+						if(data.result=="success"){
+						    newbootbox.newdialogClose("addModal");
+							newbootbox.alertInfo('保存成功！').done(function(){
+							});
+						}else{
+							newbootbox.alertInfo('保存失败！');
+						}
+					}
+				})
+		    },
+		    errorPlacement: function(error, element) {
+	    	 	if($(element).parent().hasClass("selecttree")){
+	    	 		error.appendTo(element.parent().parent().parent()); 
+	    	 	}else{
+	    	 		error.appendTo(element.parent());  
+	    	 	}
+		     }
+		});
+		
+		//保存
+		$("#save").click(function(){
+			$("#commentForm").submit();
+		});
+		
+		//重置
+		$("#reset").click(function(){
+			removeInputData([""]);
+		});
+		
+		//取消
+		$("#close").click(function(){
+			newbootbox.newdialogClose("addModal");
+		});
+	}
+
+	return{
+		//加载页面处理程序
+		initControl:function(){
+			initUnitTree();
+			initother();
+		}
+	};
+}();
+
+
+
