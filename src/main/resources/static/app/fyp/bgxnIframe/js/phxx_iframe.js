8af4d0a763ddd7d1f610ba487dd9e17a9450cbc6
lixue <lixue>
2020-09-07 11:23:10 +0800

    排行全部完成

--- src/main/resources/static/app/fyp/bgxnIframe/js/phxx_iframe.js
+++ src/main/resources/static/app/fyp/bgxnIframe/js/phxx_iframe.js
@@ -143,7 +143,7 @@ var pageModule = function () {
 						'<p>在线：'+(data.zx?data.zx:0)+'</p>'+
 						'<p>请假：'+(data.qj?data.qj:0)+'</p>'+
 						'<p>其他：'+(data.qt?data.qt:0)+'</p>'+
-						'<p>在位率：'+(data.zwl?data.zwl:0)+'</p>'+
+						'<p>在线率：'+(data.zwl?data.zwl+"%":0)+'</p>'+
 						//                    '<p><a href="index.html" target="_blank" style="color:#fff;">点击查看&nbsp;&gt;&nbsp; </a></p>'+
 						'</div>';
 					return html;
