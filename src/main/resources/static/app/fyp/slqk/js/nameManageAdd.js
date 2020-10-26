c17ab0f101c969b6f7a0fc64d5c3fc47c6df43d9
chenwenhua <1053689809@qq.com>
2020-10-26 15:21:03 +0800

    添加硬软件名称管理功能

---
+++ src/main/resources/static/app/fyp/slqk/js/nameManageAdd.js
@@ -0,0 +1,90 @@
+var saveUrl;
+var userTreeUrl = {"url":"/app/base/user/tree","dataType":"text"}; //人员树---待测试
+var returnDataUrl = {"url":"/fyp/feedbackhear/info","dataType":"text"}; //返回数据url
+var id=getUrlParam("id")||"";//编辑数据id
+if(!!id){
+	saveUrl = {"url":"/fyp/feedbackhear/update","dataType":"json"};  //edit
+}else{
+	saveUrl = {"url":"/fyp/feedbackhear/save","dataType":"json"};  //save
+}
+var saveLoading = false
+var pageModule = function(){
+	
+	var initdatafn = function(){
+		$ajax({
+			url:returnDataUrl,
+			data:{id:id},
+			success:function(data){
+				setformdata(data.data);
+			}
+		})
+	}
+	
+	var initother = function(){
+		
+		$("#commentForm").validate({
+			ignore:'',
+		    submitHandler: function() {
+				if(saveLoading){
+					newbootbox.alert('正在保存中，请稍候！')
+					return
+				}
+				saveLoading = true
+				$("#save").attr('disabled',true)
+			    var elementarry = ["status","productName"];
+				var paramdata = getformdata(elementarry);
+				paramdata.name= $('#softname').val()
+				paramdata.id = id;
+				$ajax({
+					url:saveUrl,
+					data:paramdata,
+					type:'post',
+					success:function(data){
+						if(data.result=="success"){
+						    newbootbox.newdialogClose("addModal");
+							newbootbox.alertInfo('保存成功！').done(function(){
+								window.top.pageModule.initgrid();
+							});
+						}else{
+							saveLoading = false;
+							$("#save").attr('disabled',false)
+							newbootbox.alertInfo('保存失败！');
+						}
+					}
+				})
+		    },
+		    errorPlacement: function(error, element) {
+	    	 	if($(element).parent().hasClass("selecttree")){
+	    	 		error.appendTo(element.parent().parent().parent()); 
+	    	 	}if($(element).parent().hasClass("date-picker")){
+	    	 		error.appendTo(element.parent().parent()); 
+	    	 	}else{
+	    	 		error.appendTo(element.parent());  
+	    	 	}
+		     }
+		});
+		
+		//保存
+		$("#save").click(function(){
+			$("#commentForm").submit();
+		});
+		
+		//取消
+		$("#close").click(function(){
+			saveLoading = false;
+			$("#save").attr('disabled',false)
+			newbootbox.newdialogClose("addModal");
+		});
+	}
+
+	return{
+		//加载页面处理程序
+		initControl:function(){
+			initdatafn();
+			initother();
+		}
+	};
+}();
+
+
+
