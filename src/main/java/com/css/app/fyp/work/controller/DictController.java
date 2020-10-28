c52a2be1d974a9b82c3577a3a9bb3937b332cdd4
hanwei <hanwei>
2020-10-28 13:56:29 +0800

    负一屏获取督办数据参数修改

--- src/main/java/com/css/app/fyp/work/controller/DictController.java
+++ src/main/java/com/css/app/fyp/work/controller/DictController.java
@@ -7,7 +7,9 @@ import java.util.Map;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.css.addbase.apporgan.entity.BaseAppOrgan;
+import com.css.addbase.apporgan.entity.BaseAppUser;
 import com.css.addbase.apporgan.service.BaseAppOrganService;
+import com.css.addbase.apporgan.service.BaseAppUserService;
 import com.css.addbase.apporgan.util.OrgUtil;
 import com.css.addbase.apporgmapped.service.BaseAppOrgMappedService;
 import com.css.app.fyp.utils.ResponseValueUtils;
@@ -18,6 +20,8 @@ import org.springframework.web.bind.annotation.*;
 import com.github.pagehelper.PageHelper;
 import com.css.app.fyp.work.entity.Dict;
 import com.css.app.fyp.work.service.DictService;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 /**
@@ -36,6 +40,8 @@ public class DictController {
 	private BaseAppOrganService baseAppOrganService;
 	@Autowired
 	private BaseAppOrgMappedService baseAppOrgMappedService;
+	@Autowired
+	private BaseAppUserService baseAppUserService;
 	
 	/**
 	 * 列表
@@ -115,27 +121,75 @@ public class DictController {
 	 */
 	@ResponseBody
 	@RequestMapping(value = "/findConfigDept")
-	public void findConfigDept(){
-		List<Map<String,Object>> list = dictService.findDeptids();
+	public void getDeptList(HttpServletRequest request) {
+		JSONObject result = new JSONObject();
+		JSONArray jsons = new JSONArray();
+		String organid = baseAppOrgMappedService.getBareauByUserId(CurrentUser.getUserId());
+		JSONArray list=  getUserList(organid,jsons);
+		Map<String,Object> map=new HashMap<String,Object>();
+		map.put("parentId", organid);
+		int depts_count= baseAppOrganService.queryTotal(map);
+		map=new HashMap<String,Object>();
+		map.put("departmentId", organid);
+		int users_count= baseAppUserService.queryTotal(map);
 
-		String organId = baseAppOrgMappedService.getBareauByUserId(CurrentUser.getUserId());
-		List<BaseAppOrgan> organs = baseAppOrganService.queryList(null);
-		JSONObject lists= OrgUtil.getOrganTree(organs, organId);
-		JSONArray children = (JSONArray)lists.get("children");
+		result.put("total", depts_count + users_count);
+		result.put("rows", list);
+		Response.json(result);
+	}
 
-		for (Object object :children) {
-			JSONObject jo = (JSONObject) object;
-			String dept = jo.get("id").toString();
-			for (Map<String,Object> map:list) {
-				if(dept.equals(map.get("DEPT_ID").toString())){
-					jo.put("dictType",map.get("TYPE"));
-					break;
-				}else{
-					jo.put("dictType","0");
-				}
+	public JSONArray getUserList(String id,JSONArray jsons){
+		List<BaseAppOrgan> depts = baseAppOrganService.findByParentId(id);
+		String organid = baseAppOrgMappedService.getBareauByUserId(CurrentUser.getUserId());
+		for (int i = 0;i < depts.size();i++) {
+			BaseAppOrgan dept  = depts.get(i);
+			JSONObject json = new JSONObject();
+			if (organid.equals(id)) {
+				json.put("id", dept.getId());
+				json.put("name", dept.getName());
+				json.put("rownum", i+1);
+				json.put("phone", "");
+				json.put("auth", "");
+				json.put("lx", "dept");
+				json.put("state", "closed");
+			} else {
+				json.put("id", dept.getId());
+				json.put("name", dept.getName());
+				json.put("phone", "");
+				json.put("auth", "");
+				json.put("rownum", i+1);
+				json.put("lx", "dept");
+				json.put("_parentId", id);
+				json.put("state", "closed");
 			}
+			jsons.add(json);
+			getUserList(dept.getId(),jsons);
 		}
-		Response.json(new ResponseValueUtils().success(lists));
+		if(organid.equals(id)){
+			jsonObject(jsons,id);
+		}
+		return jsons;
+	}
+
+	private void jsonObject(JSONArray jsons, String id) {
+		List<BaseAppUser> users = baseAppUserService.findByDepartmentId(id);
+		for (BaseAppUser user:users) {
+			JSONObject json = new JSONObject();
+			json.put("id", user.getId());
+			json.put("name", user.getTruename());
+			json.put("phone", user.getMobile());
+			json.put("newadd", "ok");
+			json.put("auth", "");
+			//授权
+			String roleNames = "";
+			json.put("auth",roleNames);
+			json.put("lx","user");
+			String organid = baseAppOrgMappedService.getBareauByUserId(CurrentUser.getUserId());
+			if (!organid.equals(id)) {
+				json.put("_parentId", id);
+			}
+			jsons.add(json);
+		}
 	}
 
 	/**
