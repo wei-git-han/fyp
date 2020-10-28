060bf9aee156d46ebff008aa5b3ddc36c6b93146
lixue <lixue>
2020-10-28 16:46:11 +0800

    统计单位设置修改 在编人员设置修改

--- src/main/resources/static/app/fyp/set/js/departSetting.js
+++ src/main/resources/static/app/fyp/set/js/departSetting.js
@@ -1,65 +1,7 @@
 var listurl = {"url":"/dict/findConfigDept","dataType":"text"};//表格数据
-var delUrl = {"url":"/fyp/roleedit/delete","dataType":"text"};//删除
-var deptTreeUrl = {"url":"/app/base/dept/tree","dataType":"text"}; //单位树
-var userTreeUrl = {"url":"/app/base/user/tree","dataType":"text"}; //人员树
-var url_userlist ={"url":"/roleuser/userlist","dataType":"text"}; //获取指定部门下的用户
-var grid = null;
 var hideUrl = {url:"/dict/insertConfigDept",type:'text'}
+
 var pageModule = function () {
-	/*var initgrid = function(){
-		  grid = $("#gridcont").createGrid({
-			columns:[
-						{display:"单位名称",name:"deptName",width:"70%",align:"center",render:function(rowdata){
-							return rowdata.text;
-						}},
-						{display:"是否列入统计范围",name:"do",width:"30%",align:"center",render:function(rowdata){
-							if(rowdata.dictType=='1'){
-								return '<input type="checkbox" name="doType" value="'+rowdata.id +'" id="'+rowdata.id+'" data-name="'+rowdata.text +'">'
-							}else{
-								return '<input type="checkbox" name="doType" checked value="'+rowdata.id +'" id="'+rowdata.id+'" data-name="'+rowdata.text +'">'
-							}
-						}}
-					 ],
-			width:'100%',
-			height:'100%',
-			checkbox: true,
-			rownumberyon:true,
-			paramobj:{},
-			overflowx:false,
-			pageyno:false,
-			url: listurl,
-		    loadafter:function(data){
-				  $('[name="doType"]').bootstrapSwitch({
-					  onText:'已统计',
-					  offText:'不统计',
-					  // onColor:'success',
-					  // offColor:'warning',
-					  size:'small',
-					  onSwitchChange:function (event,state) {
-						  // console.log(this.value)
-						  // console.log(this.dataset.name)
-						  // console.log(state)
-						  // var url = state?showUrl:hideUrl;
-						  var type= state?'统计状态！':'不统计状态！';
-						  var typeNum = state?'0':'1'
-						  var data = {deptids:this.value,type:typeNum};
-						  var name = this.dataset.name
-						  $ajax({
-							  url:hideUrl,
-							  data:data,
-							  success:function (res) {
-								  newbootbox.alertInfo(name+'已修改为'+type).done(function () {
-									  grid.refresh();
-								  })
-							  }
-						  })
-					  }
-				  })
-			  }
-	   });
-	}*/
-	
-	
 	var initgrid = function(){
 		$('#gridcont').treegrid({
 			columns : [ [
@@ -89,23 +31,6 @@ var pageModule = function () {
 			loadingMessage : '请等待……',
 			loadMsg : '请稍等……',
 			onBeforeExpand : function(row) {
-				var id = row.id;
-				var type = row.lx;
-				$.ajax({
-					url : url_userlist,
-					data : {id : id,type : type},
-					success : function(data) {
-						if (!data) {
-							return;
-						}
-						for ( var i in data) {
-							var id_tmp = data[i]['id'];
-							var data_tmp = data[i];
-							$('#datagrid-row-r1-2-' + id_tmp).remove();
-						}
-						$('#gridcont').treegrid('append', {parent:id,data:data});
-					}
-				});
 			},
 			onLoadSuccess:function(row){
 				$('[name="doType"]').bootstrapSwitch({
@@ -132,15 +57,10 @@ var pageModule = function () {
 		});
 	}
 	
-	var initother = function(){
-
-	}
-	
     return {
         //加载页面处理程序
         initControl: function () {
 			initgrid();
-			initother();
         },
         initgrid:function(){
             initgrid();
--- src/main/resources/static/app/fyp/zbry/js/index.js
+++ src/main/resources/static/app/fyp/zbry/js/index.js
@@ -1,222 +1,70 @@
-var ComUserList = {"url":"/dict/findConfigUser","dataType":"text"};
-var AllUserList = {"url":"/app/base/user/tree","dataType":"text"};
-var saveUrl = {"url":"/dict/insertConfigUser","dataType":"json"};
+var listurl = {"url":"/dict/findConfigUser","dataType":"text"};//表格数据
+var hideUrl = {url:"/dict/insertConfigDept",type:'text'}
 
-var checkArry=[];
-var pageModule = function(){
-	var initAllUser = function(){
-		$ajax({
-			url:AllUserList,
-			async:false,
-			success:function(data){
-				$("#tree_2").jstree("destroy");
-				$("#tree_2").jstree({
-				    "plugins": ["wholerow", "types" ,"checkbox" ],
-				    "core": {
-				    	"themes" : {
-					        "responsive": false
-					    },    
-					    "data": data,
-				    },
-				    "types" : {
-				    	"default" : {
-					        "icon" : "peoples_img"
-					    },
-					    "file" : {
-					        "icon" : "peoples_img"
-					    },
-					    "1" : {
-					        "icon" : "people_img"
-					    }
-				    }
-				});
-				$("#tree_2").on("load_node.jstree", function(e,data) {
-					for(var i=0;i<checkArry.length;i++){
-						$("#tree_2").jstree("select_node",checkArry[i],true);
-					}
-					if(checkArry.length>0){
-						toRight()
-					}
-				});
-				$("#tree_2").on("select_node.jstree", function(e,data) {
-					var nodes2 = $("#tree_2").jstree("get_bottom_selected",true);
-					ifShowToRight(nodes2);
-				});
-				$("#tree_2").on("deselect_node.jstree", function(e,data) {
-					var nodes2 = $("#tree_2").jstree("get_bottom_selected",true);
-					ifShowToRight(nodes2);
-				});
-			}
-		})
-	}
-	
-	var initComUser = function(){
-		$ajax({
-			url:ComUserList,
-			type: "GET",
-			async:false,
-			success:function(data){
-				if(data!=null){
-					$("#alreadyUser").html("");
-					loadUser(data.data);
-				}
+var pageModule = function () {
+	var initgrid = function(){
+		$('#gridcont').treegrid({
+			columns : [ [
+					{title : '序号',field : 'rownum',width : 1,align : 'center'},
+					{title : '姓名',field : 'TRUENAME',width : 10,align : 'left'},
+					{title:'是否列入统计范围',field:'dictType',width:10,align:'center',formatter:function(value,rowdata){
+						if(rowdata.dictType=='1'){
+							return '<input type="checkbox" name="doType" value="'+rowdata.id +'" id="'+rowdata.id+'" data-name="'+rowdata.name +'">'
+						}else{
+							return '<input type="checkbox" name="doType" checked value="'+rowdata.id +'" id="'+rowdata.id+'" data-name="'+rowdata.name +'">'
+						}
+			    	}}
+				 ] ],
+			width : $('#gridcont').parent().width(),
+			height : $('#gridcont').parent().height(),
+			rownumbers : false,
+			animate : true,
+			autoRowHeight : false,
+			pagination : true,
+			pageSize : 20,
+			pageList : [ 5, 10, 20, 30 ],
+			fitColumns : true,
+			url : listurl.url,
+			async : true,
+			idField : 'id',
+			treeField : 'name',
+			loadingMessage : '请等待……',
+			loadMsg : '请稍等……',
+			onBeforeExpand : function(row) {
+			},
+			onLoadSuccess:function(row){
+				$('[name="doType"]').bootstrapSwitch({
+					  onText:'已统计',
+					  offText:'不统计',
+					  size:'small',
+					  onSwitchChange:function (event,state) {
+						  var type= state?'统计状态！':'不统计状态！';
+						  var typeNum = state?'0':'1'
+						  var data = {deptids:this.value,type:typeNum};
+						  var name = this.dataset.name
+						  $ajax({
+							  url:hideUrl,
+							  data:data,
+							  success:function (res) {
+								  newbootbox.alertInfo(name+'已修改为'+type).done(function () {
+									  initgrid();
+								  })
+							  }
+						  })
+					  }
+				  })
 			}
 		});
 	}
 	
-	var initOther = function(){
-		$("#save").click(function(){
-			var idArry = [];
-			var nameArry = [];
-			console.log(nameArry);
-			$("#alreadyUser li").each(function(i,item){
-				idArry.push($(this).attr("id"));
-				nameArry.push($(this).attr("name"));
-			})
-			console.log(nameArry);
-			$ajax({
-				url:saveUrl,
-				data:{userids:idArry.toString(),nameArr:nameArry.toString()},
-				type: "post",
-				success:function(data){
-					if(data.msg == "success"){
-						newbootbox.alertInfo('保存成功！');
-					}else{
-						newbootbox.alertInfo('保存失败！');
-					}
-				}
-			});
-		});
-		
-		$("#quit").click(function(){
-			$("#alreadyUser").html("");
-			checkArry=[];
-			initAllUser();
-		});
-	}
-
-	return{
-		//加载页面处理程序
-		initControl:function(){
-			initComUser();
-			initAllUser();
-			initOther();
-		}
-	};
-	
+    return {
+        //加载页面处理程序
+        initControl: function () {
+			initgrid();
+        },
+        initgrid:function(){
+            initgrid();
+        }
+    }
 }();
-var rightList = []
-function AddUser(nodes2){
-	rightList = []
-	$.each(nodes2, function(i,obj) {
-		if(obj.original.type == 1){
-			$("#alreadyUser").append(
-				'<li id='+obj.id+' name='+obj.text+'>'+
-				'	<a href="javascript:;">'+
-				'		<span>'+obj.text+'</span>'+
-				'		<span style="float:right" class="do-icon">'+
-				'			<i class="del" onclick="deletefn(\''+obj.id+'\',this);"><img src="../../common/images/shanchu.svg" alt=""></i>'+
-				// '			<i class="up" onclick="upfn(this);"><img src="../../../../common/images/shang01.svg" alt=""></i>'+
-				// '			<i class="down" onclick="downfn(this)"><img src="../../../../common/images/xia01.svg" alt=""></i>'+
-				'		</span>'+
-				'	</a>'+
-				'</li>'
-			);
-			rightList.push(obj.id)
-		}
-	});
-	if(rightList.length>0){
-		$('.toLeft').removeClass('defaultIcon');
-		$('.toLeft').addClass('activeIcon')
-	}else{
-		$('.toLeft').removeClass('activeIcon');
-		$('.toLeft').addClass('defaultIcon')
-	}
-	console.log($("#alreadyUser").html());
-	iShow();
-}
 
-function toLeft(){
-	$("#tree_2").jstree('uncheck_all')
-	for(var i=0;i<rightList.length;i++){
-		$("#tree_2").jstree("select_node",rightList[i],true);
-	}
-}
-function toRight() {
-	$("#alreadyUser").html("");
-	var nodes2 = $("#tree_2").jstree("get_bottom_selected",true);
-	AddUser(nodes2);
-}
-function loadUser(nodes2){
-	$.each(nodes2, function(i,obj) {
-		checkArry.push(obj.USER_ID);
-	});
-}
-
-function ifShowToRight(data) {
-	var list = []
-	$.each(data, function(i,obj) {
-		if(obj.original.type == 1){
-			list.push(obj.id)
-		}
-	});
-	if(list.length>0){
-		$('.toRight').removeClass('defaultIcon');
-		$('.toRight').addClass('activeIcon')
-	}else{
-		$('.toRight').removeClass('activeIcon');
-		$('.toRight').addClass('defaultIcon')
-	}
-}
-function iShow(){
-	$("#alreadyUser li").each(function(i,item){
-		if(i==0){
-			$(this).find(".up").hide();
-		};
-		if(i==$("#alreadyUser li").length-1){
-			$(this).find(".down").hide();
-		};
-	})
-}
-
-function deletefn(id,obj){
-	// $(obj).parents("li").remove();
-	// $("#tree_2").jstree().uncheck_node(id);
-	rightList.remove(id);
-	$('#alreadyUser '+'#'+id).remove()
-	if(rightList.length>0){
-		$('.toLeft').removeClass('defaultIcon');
-		$('.toLeft').addClass('activeIcon')
-	}else{
-		$('.toLeft').removeClass('activeIcon');
-		$('.toLeft').addClass('defaultIcon')
-	}
-	iShow()
-}
-
-function upfn(obj){
-	var currentId = $(obj).parents("li").attr("id");
-	var prevId = $(obj).parents("li").prev().attr("id");
-	var currentText = $(obj).parent().prev().text();
-	var prevText = $(obj).parents("li").prev().find("span:eq(0)").text();
-	$(obj).parent().prev().text(prevText);
-	$(obj).parents("li").attr("id",prevId);
-	$(obj).parents("li").attr("name",prevText);
-	$(obj).parents("li").prev().find("span:eq(0)").text(currentText);
-	$(obj).parents("li").prev().attr("id",currentId);
-	$(obj).parents("li").prev().attr("name",currentText);
-	iShow();
-}
-function downfn(obj){
-	var currentId = $(obj).parents("li").attr("id");
-	var nextId = $(obj).parents("li").next().attr("id");
-	var currentText = $(obj).parent().prev().text();
-	var nextText = $(obj).parents("li").next().find("span:eq(0)").text();
-	$(obj).parent().prev().text(nextText);
-	$(obj).parents("li").attr("id",nextId);
-	$(obj).parents("li").attr("name",nextText);
-	$(obj).parents("li").next().find("span:eq(0)").text(currentText);
-	$(obj).parents("li").next().attr("id",currentId);
-	$(obj).parents("li").next().attr("name",currentText);
-	iShow();
-}
