3021cb59e97a2f8df414ce5b4dce75ce02de2be1
sunzh <824562329@qq.com>
2020-11-13 20:58:04 +0800

    办结率截取一位小数

--- src/main/java/com/css/app/fyp/work/ManageThingController.java
+++ src/main/java/com/css/app/fyp/work/ManageThingController.java
@@ -64,9 +64,17 @@ public class ManageThingController {
         dataMap.put("timeOutNotEnd",dataList.get(0).get("overTimewbj"));//超时未结
         dataMap.put("working",dataList.get(0).get("onTimeblz"));//时限内在办
         dataMap.put("dayNumber",dataList.get(0).get("aveDays"));//平均办理天数
-        dataMap.put("percentage",dataList.get(0).get("wcl"));//办结率
+        String bjl = dataList.get(0).get("wcl").toString();
+        bjl = bjl.substring(0,bjl.lastIndexOf(".")+2);
+        dataMap.put("percentage",bjl);//办结率
         dataMap.put("total",dataList.get(0).get("zsl"));//督办总量
         Response.json(new ResponseValueUtils().success(dataMap));
+    }
+
+    public static void main(String args[]){
+        String bjl = "1321.212";
+        bjl = bjl.substring(0,bjl.lastIndexOf(".")+2);
+        System.out.println(bjl);
     }
 
     /**
