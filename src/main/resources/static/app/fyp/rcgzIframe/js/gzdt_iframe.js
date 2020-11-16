f879934c2f60ba6455acaf745c8a1fe99f0b04bc
宋华健 <2240255905@qq.com>
2020-11-16 16:25:54 +0800

    1.首长待办修改及周表调整

--- src/main/resources/static/app/fyp/rcgzIframe/js/gzdt_iframe.js
+++ src/main/resources/static/app/fyp/rcgzIframe/js/gzdt_iframe.js
@@ -8,14 +8,16 @@ var pageModule = function () {
 			success: function(data) {
 				var arryHtml = '';
 				$.each(data.data.recentnews, function(i, o) {
+					if(i<=2){
 					arryHtml+=`<dl onclick="openById('${data.data.appId}','${o.id}','${data.data.ip}','${i}')" style="cursor: pointer">
-									<dt style="overflow: hidden" ><img src="${o.coverImg?data.data.ip+o.coverImg+top.location.search:'/app/fyp/common/images/u11814.svg'}" style="width: 100%;height: 100%"></dt>
-									<dd>
-										<div class="title">${o.title}</div>
-										<div class="content">${o.content}</div>
-										<div class="btns"><span class="date">${o.createDate}</span>阅读<span class="num">${o.hits}</span><a id="${o.id}">查看详情</a></div>
-									</dd>
-								</dl>`
+								<dt style="overflow: hidden" ><img src="${o.coverImg?data.data.ip+o.coverImg+top.location.search:'/app/fyp/common/images/u11814.svg'}" style="width: 100%;height: 100%"></dt>
+								<dd>
+									<div class="title">${o.title}</div>
+									<div class="content">${o.content}</div>
+									<div class="btns"><span class="date">${o.createDate}</span>阅读<span class="num">${o.hits}</span><a id="${o.id}">查看详情</a></div>
+								</dd>
+							</dl>`
+					}
 				});
 				$("#gzdt").html(arryHtml);
 			}
